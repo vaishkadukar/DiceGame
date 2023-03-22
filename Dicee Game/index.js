99b90ccc234cd2d289342d4a6ed27cba5556b8aa
Vaishnavi Kadukar <113694666+vaishkadukar@users.noreply.github.com>
2023-03-22 17:01:23 +0530

    1st commit

---
+++ Dicee Game/index.js
@@ -0,0 +1,29 @@
+let randomNumber1 = Math.floor(Math.random() * 6)+1;
+let randomNumber2 = Math.floor(Math.random() * 6)+1;
+// alert(randomNumber1+" , "+randomNumber2);
+
+switch(randomNumber1){
+    case 1: document.querySelector(".img1").setAttribute("src","images/dice1.png");break;
+    case 2: document.querySelector(".img1").setAttribute("src","images/dice2.png");break;
+    case 3: document.querySelector(".img1").setAttribute("src","images/dice3.png");break;
+    case 4: document.querySelector(".img1").setAttribute("src","images/dice4.png");break;
+    case 5: document.querySelector(".img1").setAttribute("src","images/dice5.png");break;
+    case 6: document.querySelector(".img1").setAttribute("src","images/dice6.png");break;
+}
+
+switch(randomNumber2){
+    case 1: document.querySelector(".img2").setAttribute("src","images/dice1.png");break;
+    case 2: document.querySelector(".img2").setAttribute("src","images/dice2.png");break;
+    case 3: document.querySelector(".img2").setAttribute("src","images/dice3.png");break;
+    case 4: document.querySelector(".img2").setAttribute("src","images/dice4.png");break;
+    case 5: document.querySelector(".img2").setAttribute("src","images/dice5.png");break;
+    case 6: document.querySelector(".img2").setAttribute("src","images/dice6.png");break;
+}
+
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").textContent ="Draw!";
+}else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent ="Player 1 Wins!";
+}else{
+    document.querySelector("h1").textContent ="Player 2 Wins!";
+}
